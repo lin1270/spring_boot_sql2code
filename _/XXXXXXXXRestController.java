ca7a0803571850bbf5b1e8e6a0d1bd2a1ce30cd0
linqingchang <linqingchang@linqingchang>
2019-06-04 18:09:57 +0800

    添加save/remove

--- _/XXXXXXXXRestController.java
+++ _/XXXXXXXXRestController.java
@@ -4,10 +4,12 @@ import com.pactera.jep.autoconfigure.web.controller.BaseRestController;
 import com.pactera.jep.orm.Filter;
 import com.pactera.jep.orm.Order;
 import com.pactera.jep.orm.Page;
+import com.pactera.jep.core.exception.ServiceException;
 import com.pactera.jep.service.base.model.XXXXXXXX;
 import com.pactera.jep.service.base.service.XXXXXXXXService;
 import com.pactera.jep.web.code.WebCode;
 import com.pactera.jep.web.code.WebHeaderCode;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiModelProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,4 +37,56 @@ public class XXXXXXXXRestController extends BaseRestController<XXXXXXXX> {
     protected void prepareQuery(@ApiIgnore @RequestHeader(WebHeaderCode.X_USER_TOKEN) String token, Page<XXXXXXXX> page, List<Filter> filters, List<Order> orders, HttpServletRequest request) {
         service.query(page, filters, orders);
     }
+
+    @ApiOperation(value = "保存一条数据")
+	@PostMapping(value = "/save")
+	public Map<String, Object> save(@RequestBody XXXXXXXX saveInfo) {
+		Map<String, Object> tips = new HashMap<>();
+
+		try
+		{
+			boolean	 exists = service.exists(new Filter.Builder().eq("id", saveInfo.getId()).build());
+			if(exists)
+			{
+				service.update(saveInfo);
+				tips.put(WebCode.SUCCESS, true);
+				tips.put(WebCode.MSG, WebCode.SUCCESS_SAVE);
+			}else {
+				service.insert(saveInfo);
+				tips.put(WebCode.SUCCESS, true);
+				tips.put(WebCode.MSG, WebCode.SUCCESS_SAVE);
+			}
+		}
+		catch (ServiceException e)
+		{
+			logger.error("error:",e);
+			tips.put(WebCode.SUCCESS, false);
+			tips.put(WebCode.MSG, WebCode.FAILURE_SAVE);
+		}
+		return tips;
+	}
+
+	/**
+	 * 删除
+	 * @param id id
+	 * @return map
+	 */
+	@ApiOperation(value = "根据编码删除一条数据")
+	@DeleteMapping(value = "/remove/{id}")
+	public Map<String, Object> remove(@PathVariable String id){
+		Map<String, Object> tips = new HashMap<>();
+		try
+		{
+			service.deleteByPrimaryKey(id);
+			tips.put(WebCode.SUCCESS, true);
+			tips.put(WebCode.MSG, WebCode.SUCCESS_DELETE);
+		}
+		catch (ServiceException e)
+		{
+			logger.error("error:",e);
+			tips.put(WebCode.SUCCESS, false);
+			tips.put(WebCode.MSG, WebCode.FAILURE_DELETE);
+		}
+		return tips;
+	}
 }
